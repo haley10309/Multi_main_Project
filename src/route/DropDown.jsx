ccde73088d34c0a73135349422c93cc2fe60c455
hayung <yoohayung10309@gmail.com>
2024-04-11 14:56:21 +0900

    git ignore 생성

---
+++ src/route/DropDown.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DropDown = () => {
+  return (
+    <>
+      <li>최신순</li>
+      <li>인기순</li>
+    </>
+  );
+};
+
+export default DropDown;
--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import './Home.scss';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Home.scss";
+import { Link, useNavigate } from "react-router-dom";
+import DropDown from "./DropDown";
 
 const Home = () => {
-    const navigate = useNavigate();
-    
-    return (
-        <div className='home_body'>
-            
-            
-            <br/>
-            <h1 className='IT_NEWS'>IT NEWS</h1>
-        </div>
-    );
+  const navigate = useNavigate();
+  const [view, setView] = useState(false);
+  
+  return (
+    <div className="home_body">
+      <h1 className="IT_NEWS">IT NEWS</h1>
+      <ul className="dropdown_menu"
+        onClick={() => {
+          setView(!view);
+        }}
+      >
+        최신순 {view ? "⌃" : "⌄"}
+        {view && <DropDown />}
+      </ul>
+
+    </div>
+  );
 };
 
 export default Home;
