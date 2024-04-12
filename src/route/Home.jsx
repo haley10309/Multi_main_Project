10579a2ec19779d1da41c3be0a7f28f838286c41
hayung <yoohayung10309@gmail.com>
2024-04-12 14:14:49 +0900

    home profile name box refix

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -6,19 +6,32 @@ import DropDown from "./DropDown";
 const Home = () => {
   const navigate = useNavigate();
   const [view, setView] = useState(false);
-  
+
   return (
     <div className="home_body">
       <h1 className="IT_NEWS">IT NEWS</h1>
-      <ul className="dropdown_menu"
-        onClick={() => {
-          setView(!view);
-        }}
-      >
-        최신순 {view ? "⌃" : "⌄"}
-        {view && <DropDown />}
-      </ul>
+      <div className="">
+        <ul
+          className="dropdown_menu"
+          onClick={() => {
+            setView(!view);
+          }}
+        >
+          최신순 {view ? "⌃" : "⌄"}
+          {view && <DropDown />}
+        </ul>
+      </div>
 
+      <div className="posting_box">
+        <div className="profile_box">
+          <img
+            src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYLJauBsuuhYaRAYccQZ2d-UtBTCOgsHMQmw&s`}
+            className="profile_img"
+            alt="React"
+          />
+          <h4 className="profile_name">owenDorothy</h4>
+        </div>
+      </div>
     </div>
   );
 };
