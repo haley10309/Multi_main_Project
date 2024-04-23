972598914d0f978b221ad3e12b9f966f8b76c60a
hayung <yoohayung10309@gmail.com>
2024-04-23 16:41:18 +0900

    server useEffect code

--- src/administrator/Manage_member.jsx
+++ src/administrator/Manage_member.jsx
@@ -3,70 +3,41 @@ import "./Manage_member.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
 
-
-
 const columns = [
-  { field: "id", headerName: "ID", width: 100 },
-  { field: "Name", headerName: "user_name", width: 130 },
-  { field: "NickName", headerName: "user_nickname", width: 200 },
-  { field: "age", headerName: "Age",   type: "number", width: 100, },
-  {
-    field: "JoinDate",
-    headerName: "date_of_assign",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
+  { field: "user_id", headerName: "ID", width: 100 },
+  { field: "nickname", headerName: "user_name", width: 130 },
+  { field: "email", headerName: "email", width: 200 },
+  { field: "가입일", headerName: "JoinDate", width: 160 },
 ];
 
-const row_example = [
-  { id: 1, Name: "Snow", NickName: "Jon", age: 35 , JoinDate:"2023-01-01"},
-  { id: 2, Name: "Lannister", NickName: "Cersei", age: 42 ,JoinDate:"2023-01-01"},
-  { id: 3, Name: "Lannister", NickName: "Jaime", age: 45 ,JoinDate:"2023-01-01"},
-  { id: 4, Name: "Stark", NickName: "Arya", age: 16,JoinDate:"2023-01-01" },
-  { id: 5, Name: "Targaryen", NickName: "Daenerys", age: null,JoinDate:"2023-01-01" },
-  { id: 6, Name: "Melisandre", NickName: null, age: 150,JoinDate:"2023-01-01" },
-  { id: 7, Name: "Clifford", NickName: "Ferrara", age: 44,JoinDate:"2023-01-01" },
-  { id: 8, Name: "Frances", NickName: "Rossini", age: 36,JoinDate:"2023-01-01"},
-  { id: 9, Name: "Roxie", NickName: "Harvey", age: 65 ,JoinDate:"2023-01-01"},
-];
 const Manage_member = () => {
-
-
   const [rows, setRows] = useState([]);
 
-
-
   useEffect(() => {
-    // Fetch data from server and setRows when component mounts
-    axios.get("URL_TO_YOUR_ENDPOINT")
-      .then(response => {
+    axios
+      .get("/admin/Manage_member")
+      .then((response) => {
         setRows(response.data);
+        console.log(rows);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []); 
+  }, []);
 
-
-  
   return (
     <div className="whole_box">
-        <h2 className="title" >회원 관리</h2>
+      <h2 className="title">회원 관리</h2>
       <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
-        className="data_chart"
-          rows={row_example}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
-          pageSizeOptions={[5, 10]}
-          checkboxSelection
-        />
+        {rows.map((row_cell) => {
+          <DataGrid
+            className="data_chart"
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          />;
+        })}
       </div>
     </div>
   );
--- src/administrator/Report.jsx
+++ src/administrator/Report.jsx
@@ -1,55 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import './Report.scss';
+import React, { useState, useEffect } from "react";
+import "./Report.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
 
-
-
-
 const columns = [
-    { field: "id", headerName: "ID", width: 100 },
-    { field: "Name", headerName: "user_name", width: 130 },
-    { field: "NickName", headerName: "user_nickname", width: 200 },
-    { field: "age", headerName: "Age",   type: "number", width: 100, },
-    {
-      field: "JoinDate",
-      headerName: "date_of_assign",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      // valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-    },
-  ];
-  
-  const row_example = [
-    { id: 1, Name: "Snow", NickName: "Jon", age: 35 , JoinDate:"2023-01-01"},
-    { id: 2, Name: "Lannister", NickName: "Cersei", age: 42 ,JoinDate:"2023-01-01"},
-    { id: 3, Name: "Lannister", NickName: "Jaime", age: 45 ,JoinDate:"2023-01-01"},
-    { id: 4, Name: "Stark", NickName: "Arya", age: 16,JoinDate:"2023-01-01" },
-    { id: 5, Name: "Targaryen", NickName: "Daenerys", age: null,JoinDate:"2023-01-01" },
-    { id: 6, Name: "Melisandre", NickName: null, age: 150,JoinDate:"2023-01-01" },
-    { id: 7, Name: "Clifford", NickName: "Ferrara", age: 44,JoinDate:"2023-01-01" },
-    { id: 8, Name: "Frances", NickName: "Rossini", age: 36,JoinDate:"2023-01-01"},
-    { id: 9, Name: "Roxie", NickName: "Harvey", age: 65 ,JoinDate:"2023-01-01"},
-  ];
-const Report = () => {
-    const [rows, setRows] = useState([]);
-    useEffect(() => {
-        // Fetch data from server and setRows when component mounts
-        axios.get("URL_TO_YOUR_ENDPOINT")
-          .then(response => {
-            setRows(response.data);
-          })
-          .catch(error => {
-            console.error("Error fetching data:", error);
-          });
-      }, []); 
+  { field: "id", headerName: "ID", width: 100 },
+  { field: "Name", headerName: "user_name", width: 130 },
+  { field: "NickName", headerName: "user_nickname", width: 200 },
+  { field: "Email", headerName: "email", width: 100 },
+  {
+    field: "JoinDate",
+    headerName: "date_of_assign",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+  },
+];
 
-    return (
-        <div className="whole_box">
-        <h2 className="title" >신고</h2>
-      <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
+
+const row_example = [
+  {
+    id: 1,
+    Name: "Snow",
+    NickName: "Jon",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 2,
+    Name: "Lannister",
+    NickName: "Cersei",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 3,
+    Name: "Lannister",
+    NickName: "Jaime",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 4,
+    Name: "Stark",
+    NickName: "Arya",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 5,
+    Name: "Targaryen",
+    NickName: "Daenerys",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 6,
+    Name: "Melisandre",
+    NickName: null,
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 7,
+    Name: "Clifford",
+    NickName: "Ferrara",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 8,
+    Name: "Frances",
+    NickName: "Rossini",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+  {
+    id: 9,
+    Name: "Roxie",
+    NickName: "Harvey",
+    Email: "GameOfThrones@gmail.com",
+    JoinDate: "2023-01-01",
+  },
+];
+
+const Report = () => {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    // Fetch data from server and setRows when component mounts
+    axios
+      .get("URL_TO_YOUR_ENDPOINT")
+      .then((response) => {
+        setRows(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div className="whole_box">
+      <h2 className="title">신고</h2>
+      <div className="data-grid-container">
+      <DataGrid
         className="data_chart"
           rows={row_example}
           columns={columns}
@@ -63,7 +116,7 @@ const Report = () => {
         />
       </div>
     </div>
-    );
+  );
 };
 
 export default Report;
