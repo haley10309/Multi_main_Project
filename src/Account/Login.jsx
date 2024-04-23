7f38b8b3f44382bc086e0a74808dba7f1ab19ba1
ohjaechang <ertiop0708@naver.com>
2024-04-23 17:47:51 +0900

    commit

--- src/Account/Login.jsx
+++ src/Account/Login.jsx
@@ -3,64 +3,62 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './Join_Login.scss';
 
-function Login(props) {
-  const [useraccount, setUseraccount] = useState('');
+function Login() {
+  //초기값 설정
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [msg, setMsg] = useState('');
+  const [message, setMessage] = useState('');
+  const [isEmail, setIsEmail] = useState(false);
+  const [emailMessage, setEmailMessage] = useState('');
   const navigate = useNavigate();
+  //이벤트 핸들러
+  const handleEmail = (e) => {
+    const currentEmail = e.target.value;
+    setEmail(currentEmail);
+    const emailRegExp =
+      /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
 
-  const onIDhandler = (event) => {
-    setUseraccount(event.target.value);
+    if (!emailRegExp.test(currentEmail)) {
+      setEmailMessage('이메일 형식을 확인하세요');
+      setIsEmail(false);
+    } else {
+      setEmailMessage('적합한 이메일 입니다');
+      setIsEmail(true);
+    }
   };
-  const onPwhandler = (event) => {
-    setPassword(event.target.value);
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  //jwt 로그인 방식
+  const handleLogin = async () => {
+    return await axios.post('url', {
+      email: 'email',
+      password: 'password',
+    });
   };
 
-  // useEffect=(()=>{
-
-  // },[mgs])
-  const LoginFunc = () => {
-    let body = {
-      useraccount,
-      password,
-    };
-    // axios.post("/login", body).then((res) => {
-    //   console.log(res.data);
-
-    //   if (res.data.code === 200) {
-    //     console.log("로그인");
-
-    //     navigate('/',{
-    //       state:useraccount
-    //     });
-
-    //   }
-    //   if (res.data.code === 404) {
-    //     setMsg("로그인 실패");
-    //   }
-
-    // });
-  };
+  //localStorage.setItem('accessToken', headers['access']);
+  //localStorage.setItem('refreshToken', headers['refresh']);
 
   return (
     <div className="login_body">
       <h1>DevOz</h1>
-      <form onSubmit={LoginFunc}>
-        <div className="ID_form_align">
-          <label htmlFor="id"> ID: </label>
-          <input type="text" id="id" onChange={onIDhandler} />
+      <form onSubmit={handleLogin}>
+        <div className="email_form_align">
+          <label htmlFor="email"> E-mail: </label>
+          <input type="text" onChange={handleEmail} />
+          <p>{emailMessage}</p>
         </div>
         <br />
         <div className="PW_form_align">
           <label htmlFor="password">PASSWORD : </label>
-          <input type="password" onChange={onPwhandler} />
+          <input type="password" onChange={handlePassword} />
         </div>
         <br />
         <button className="Login_button" type="submit">
           로그인
         </button>
         <br />
-        {msg}
       </form>
     </div>
   );
